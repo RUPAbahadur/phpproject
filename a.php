4fc0d222c18c321c4d3d5094f2ca0876eb5b1254
RUPAbahadur <37900277+RUPAbahadur@users.noreply.github.com>
2018-03-29 16:39:24 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hey, there";
+
+date_default_timezone_set(UTC);
+    echo date('h:i:s a l F j S Y ');
+
+
+
+
+?>
---
+++ fristpage.php
@@ -0,0 +1,43 @@
+<html>
+
+  <body>
+
+<? php
+   echo "hey, there";
+?>
+
+     <form action="learnphp.php method="post">
+
+         <table border="0">
+
+             <tr>
+             <td>NAME</td>
+             <td align="center"><input type="text" name="Username" size="30" />
+             </td>
+             </tr>
+	     
+	     <tr>
+             <td>ADDRESS</td>
+             <td align="center"><input type="text" name="streetaddress" size="30" />
+             </td>
+             </tr>
+
+	     <tr>
+             <td>CITY</td>
+             <td align="center"><input type="text" name="city" size="30" />
+             </td>
+             </tr>
+
+	     <tr>
+            
+             <td  colspan="2" align="center"><input type="submit" value="submit" />
+             </td>
+             </tr>
+
+        </table>
+
+     </form>
+
+   </body>
+
+</html>
